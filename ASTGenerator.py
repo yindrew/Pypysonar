246e3f29c7743cfc5f0eb900023becf92e46c2e2
Andrew Yin <yindrew@vt.edu>
2021-08-15 04:04:23 -0400

    added comments to all the code

--- ASTGenerator.py
+++ ASTGenerator.py
@@ -1,7 +1,7 @@
 import ast
 import ASTParser as parser
 
-
+# reads ToBeIndexed and parses it into a AST and then turns it into a namespace heiarchy and prints it out
 def show_ast():
     with open("ToBeIndexed.py", "r") as source:
         text = source.read()
@@ -10,8 +10,8 @@ def show_ast():
         walk(namespace, tree)
         print(namespace)
 
+# Turns a AST into a namespace heiarchy.
 def walk(namespace, node, level = 0, sep = '\t'):
-    print(namespace)
     name = parser.getName(node)
     position = parser.getPosition(node)
 
@@ -19,13 +19,10 @@ def walk(namespace, node, level = 0, sep = '\t'):
         parser.index(namespace, name, position)
 
     for n in ast.iter_child_nodes(node):
+
         if parser.shouldCreateNamespace(node):
             ns = {}
             parser.createNamespace(namespace, ns, name, position)
             walk(ns, n, level + 1)
         else:
             walk(namespace, n, level + 1)
-
-
-
-
--- ASTParser.py
+++ ASTParser.py
@@ -1,7 +1,7 @@
 import ast
 
 
-
+#returns the name of the node
 def getName(node):
     if isinstance(node, (ast.FunctionDef, ast.ClassDef)):
         return node.name
@@ -12,17 +12,17 @@ def getName(node):
     else:
         return None
 
-
+# if the node is part of builtin, dont return it
 def getNameNodeId(node):
     if isBuiltin(node.id):
         return None
     return node.id
 
-
+# return whether or not the name is part of builtin
 def isBuiltin(name):
     return name in ["print"]
 
-
+# get the position of the node
 def getPosition(node):
     if shouldIndex(node):
         startPointOffset = getStartPointOffset(node)
@@ -34,7 +34,7 @@ def getPosition(node):
     else:
         return None
 
-
+# get the start offset of the node
 def getStartPointOffset(node):
     if isinstance(node, ast.FunctionDef):
         return 4
@@ -44,23 +44,24 @@ def getStartPointOffset(node):
         return 0
 
 
+#return whether or not the node should be indexed
 def shouldIndex(node):
     #validNodeType = isinstance(node, (ast.FunctionDef, ast.arg, ast.Name))
     return getName(node) is not None
 
 
-
+#create a namespace for a node with the name as the key and the position as the value. If the key is already in, append the position on the end.
 def index(namespace, name, position):
     try:
         namespace[name].append(position)
     except:
         namespace[name] = [position]
 
-
+# create a new sub ns for nodes that have sub-namespaces
 def shouldCreateNamespace(node):
     return isinstance(node, (ast.FunctionDef, ast.ClassDef))
 
-
+# create a new subNS with the properties of the node, and then add the subNS back into the overarching NS.
 def createNamespace(namespace, newNS, name, position):
     newNS[name] = position
     namespace["NS" + name] = newNS
